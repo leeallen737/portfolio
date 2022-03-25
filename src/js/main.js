7660ec80f9a408d52f3481d3c7d372b58bcb91e8
Lee J Allen <leeallen737@gmail.com>
2022-03-25 19:48:59 +0000

    lots of content and styling added to contact and resume, plus JS added

---
+++ src/js/main.js
@@ -0,0 +1,17 @@
+const hamburger = document.querySelector('.nav-right')
+const slideMenu = document.querySelector('.side-navigation')
+
+
+
+
+hamburger.addEventListener('click', () => {
+    slideMenu.classList.toggle('open')
+})
+
+slideMenu.addEventListener('click', () => {
+    slideMenu.classList.remove('open')
+})
+
+document.addEventListener("DOMContentLoaded", () => {
+    slideMenu.classList.remove('open')
+});
